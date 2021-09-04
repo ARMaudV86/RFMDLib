2381111c8100e9bdad5d7bb454f4db7e5c768769
arnaud voisin <arnaudvividu26@gmail.com>
2021-09-04 17:25:22 +0200

    Coded register burst write in secure mode (without modification of source data) burst write seems to be functionnal as it succefully parameter frequency in the test code

--- src/spi_DIY.h
+++ src/spi_DIY.h
@@ -8,11 +8,12 @@ static int segmentationDelay;
 
 
 void spiSet(int slaveSelectPin);
-byte spiFrame8(bool Write, byte address);
-int  spiFrame(bool Write, byte address, byte data);
-void spiSegmentationStart();
-void spiSegmentationEnd();
+uint8_t spiFrame8(bool Write, byte address);
+uint16_t  spiFrame(bool Write, byte address, byte data);
+void spiSegmentationStart(void);
+void spiSegmentationEnd(void);
 void spiWriteRegister(uint8_t address, uint8_t data);
+
 uint8_t spiReadRegister(uint8_t address);
 
 
@@ -28,40 +29,56 @@ void spiSet(int slaveSelectPinP, int segmentationDelayP)
       SPI.setBitOrder(MSBFIRST);
 }
 
-byte spiFrame8(bool Write, byte address)
+uint8_t spiFrame8(bool Write, byte address)
 {
-      byte retour =0;
+      uint8_t retour =0;
       retour = Write << 7;
       retour += (address&0x7F);
       return retour;
 }
 
-int spiFrame(bool Write, byte address, byte data)
+uint16_t spiFrame(bool Write, byte address, byte data)
 {
-      int retour =0;
+      uint16_t retour =0;
       retour = Write << 15;
       retour += (address&0x7F) << 8;
       retour += data;
       return retour;
 }
 
-void spiSegmentationStart()
+void spiSegmentationStart(void)
 {
       digitalWrite(slaveSelectPin, LOW);
       delay(2*segmentationDelay);
 }
 
-void spiSegmentationEnd()
+void spiSegmentationEnd(void)
 {
       delay(segmentationDelay);
       digitalWrite(slaveSelectPin, HIGH);
 }
 
 void spiWriteRegister(uint8_t address, uint8_t data)
+//Optimisable SPECIAL CASE OF BURST WRITE
 {
     spiSegmentationStart();
 	SPI.transfer16(spiFrame(1, address, data));
     spiSegmentationEnd();
+}
+
+
+void spiBurstWriteRegister(uint8_t address, void *data, size_t dataSize)
+{
+	void *dataBis = malloc(dataSize * sizeof(void));
+	memcpy(dataBis, data, dataSize* sizeof(void));
+	// Theses sausages are used to compensate for the arduino library modifying the data array
+	// find a way to compensate for multiple malloc ? having a part of memory just for BurstWrite ?
+	
+	spiSegmentationStart();
+	SPI.transfer(spiFrame8(true, address));
+	SPI.transfer(dataBis, dataSize);
+	spiSegmentationEnd();
+	free(dataBis);
 }
 
 
